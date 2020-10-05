a7472b334bf153dc38373b90f7daefff665f23c3
Ana_stasya_S <anastasiya-s@live.de>
2020-10-05 20:26:04 +0300

    feat: part solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!arr) return new Error('error');
+  if (!Array.isArray(arr)) return new Error();
+  if (!arr.includes('--discard-prev') && !arr.includes('--discard-prev')) return arr;
+  let toChange = [...arr];
+
+  toChange.forEach((item, i, array) => {
+    if (item === '--discard-prev') {
+      if( i === 0 ) {
+
+      	array.splice(i, 1);
+        return
+      }
+      array.splice(i - 1, 2)
+      return
+    }
+    if (item === '--double-prev') {
+      if( i === 0 ) {
+      	array.splice(i, 1);
+        return
+      }
+      array.splice(i, 1, array[i - 1])
+      return
+    }
+	  if (item === '--discard-next') {
+      if (i === array.length) {
+    	  array.splice(i, 1);
+    		return
+ 			}
+      array.splice(i, 2)
+    }
+    if (item === '--double-next') {
+      if (i === array.length) {
+      	array.splice(i, 1)
+      	return;
+      }
+      array.splice(i, 2, array[i + 1])
+      return
+    } 
+  })
+  console.log(toChange)
 };
